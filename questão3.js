8583afbaef4dc52420e102fef73c5e7e88d070c5
George Neves <georgeneves97@gmail.com>
2021-12-01 16:02:45 -0300

    Criada a questão3
    Foi  utilizado o .filter para criar um novo vetor com todos os itens do vetor original porém sem os itens indesejados.

--- questão3.js
+++ questão3.js
@@ -1,2 +1,11 @@
 let array = [1,2,'', undefined]
 
+function cleararray(){
+
+    let arrayClean = array.filter(function(i){
+        return i;
+    }) 
+    console.log(array)
+    console.log(arrayClean)
+}
+cleararray()
