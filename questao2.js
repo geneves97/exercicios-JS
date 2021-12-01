e2043b4d07572f82ed0539eb1180803c6994a6d0
George Neves <georgeneves97@gmail.com>
2021-12-01 15:50:59 -0300

    melhora questão2
    foi transformado em função.

--- questao2.js
+++ questao2.js
@@ -1,9 +1,13 @@
 let array = [1, 2, 3, 4];
 
+function fliparray(){
 let invertedarray = array.map(function (item, indice, array){
   return array[array.length - indice - 1];
 })
 
 console.log(array);
-
 console.log(invertedarray);
+
+}
+fliparray()
+
