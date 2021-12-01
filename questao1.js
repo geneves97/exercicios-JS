a01a94f083b3ef8c33c4a44b344e466168602e36
George Neves <georgeneves97@gmail.com>
2021-12-01 15:11:21 -0300

    primeira questão
    foi feita a primeira questão utilizado o metodo push dentro de um while

---
+++ questao1.js
@@ -0,0 +1,16 @@
+let array = [];
+
+let conteudo
+let mult
+
+function adiciona(mult, conteudo){    
+     
+    while(array.length <= mult -1){
+
+        array.push(conteudo)
+        console.log(array);
+    }
+    
+}
+
+adiciona(3,'a')
